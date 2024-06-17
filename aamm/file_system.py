7895679cbca40c4d99e2a881b926640009d0acc0
Antonio Medellín <amedellinm@outlook.com>
2024-06-16 21:11:14 -0600

    -m

--- aamm/file_system.py
+++ aamm/file_system.py
@@ -1,11 +1,11 @@
 import filecmp
 import inspect
 import os
-import re
 from pathlib import Path
 from typing import Callable, Generator
 
-from aamm.std import depth_first
+from aamm.std import breadth_first, depth_first
+from aamm.strings import match
 
 
 def are_directories_equal(d1: str, d2: str) -> bool:
@@ -21,37 +21,67 @@ def are_directories_equal(d1: str, d2: str) -> bool:
     return True
 
 
-def current_filename(ext: str | None = ..., stack_index: int = 1) -> str:
-    filename = os.path.basename(inspect.stack()[stack_index].filename)
-    if ext is ...:
-        return filename
-    name = os.path.splitext(filename)[0]
-    if isinstance(ext, str):
-        return name + "." + ext
-    return name
+def current_file(
+    extension: str | None = ..., name_only: bool = False, stack_index: int = 1
+) -> str:
+    file = inspect.stack()[stack_index].filename
+    if name_only:
+        file = os.path.basename(file)
+
+    if extension is ...:
+        return file
+
+    name = os.path.splitext(file)[0]
+    if extension is None:
+        return name
+
+    return name + "." + extension
 
 
-def current_filepath(stack_index: int = 1) -> str:
-    return inspect.stack()[stack_index].filename
+def current_folder(name_only: bool = False, stack_index: int = 1) -> str:
+    folder_path = os.path.dirname(inspect.stack()[stack_index].filename)
+    return os.path.basename(folder_path) if name_only else folder_path
 
 
-def current_foldername(stack_index: int = 1) -> str:
-    return os.path.basename(os.path.dirname(inspect.stack()[stack_index].filename))
-
-
-def current_folderpath(stack_index: int = 1) -> str:
-    return os.path.dirname(inspect.stack()[stack_index].filename)
-
-
-def dir_up(path: str | Path = None, n: int = 1) -> str:
+def dir_up(path: str | Path = None, n: int = 1, stack_index: int = 2) -> str:
     """Returns `n` directories up the given path."""
     if path is None:
-        path = current_folderpath(2)
+        path = current_folder(stack_index=stack_index)
     elif os.path.isfile(path):
         path = os.path.dirname(path)
     for _ in range(n):
         path = os.path.dirname(path)
     return path
+
+
+def files(
+    path: str = None, names_only: bool = False, stack_index: int = 2
+) -> list[str]:
+    """List all files in `path`."""
+    if path is None:
+        path = current_folder(stack_index=stack_index)
+    try:
+        root, _, subpaths = next(os.walk(path))
+    except StopIteration:
+        return []
+    if names_only:
+        return subpaths
+    return [os.path.join(root, subpath) for subpath in subpaths]
+
+
+def folders(
+    path: str = None, names_only: bool = False, stack_index: int = 2
+) -> list[str]:
+    """List all folders in `path`."""
+    if path is None:
+        path = current_folder(stack_index=stack_index)
+    try:
+        root, subpaths, _ = next(os.walk(path))
+    except StopIteration:
+        return []
+    if names_only:
+        return subpaths
+    return [os.path.join(root, subpath) for subpath in subpaths]
 
 
 def has_extension(path: str, extension: str = None) -> bool:
@@ -60,52 +90,30 @@ def has_extension(path: str, extension: str = None) -> bool:
     return bool(ext) if extension is None else extension == ext
 
 
-def here(file_name: str = "") -> str:
+def here(file_name: str = "", stack_index: int = 2) -> str:
     """Constructs the path of a file in the current directory."""
-    return os.path.join(current_folderpath(2), file_name)
-
-
-def listfiles(path: str | Path = None) -> list[str]:
-    """List all files in `path`."""
-    if path is None:
-        path = current_folderpath(2)
-    try:
-        root, _, files = next(os.walk(path))
-    except StopIteration:
-        return []
-    return [os.path.join(root, file) for file in files]
-
-
-def listfolders(path: str | Path = None) -> list[str]:
-    """List all folders in `path`."""
-    if path is None:
-        path = current_folderpath(2)
-    try:
-        root, folders, _ = next(os.walk(path))
-    except StopIteration:
-        return []
-    return [os.path.join(root, folder) for folder in folders]
+    return os.path.join(current_folder(stack_index=stack_index), file_name)
 
 
 def search(
     root: str = None,
     condition: str | Callable = ".",
     use_complement: bool = False,
+    use_breadth_first: bool = False,
+    stack_index: int = 2,
 ) -> Generator:
     """From `root`, depth-first traverses folders according to `condition`."""
     if root is None:
-        root = current_folderpath(2)
+        root = current_folder(stack_index=stack_index)
     elif root is ...:
         root = os.getcwd()
 
     if isinstance(condition, str):
-        pattern = re.compile(condition).match
+        condition = match.create_matcher(condition)
 
-        def condition(path: str | Path) -> bool:
-            return bool(pattern(os.path.basename(path)))
+    def expand(node):
+        if condition(node) ^ use_complement:
+            return folders(node)
+        return []
 
-    queue = [root]
-    while queue:
-        if condition(node := queue.pop()) ^ use_complement:
-            queue.extend(reversed(listfolders(node)))
-            yield node
+    return (breadth_first if use_breadth_first else depth_first)(root, expand)
