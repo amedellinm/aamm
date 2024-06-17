be120cf261410f6bc5298225b49f4f9ff5614f19
Antonio Medellín <amedellinm@outlook.com>
2024-06-16 21:09:54 -0600

    -d

--- aamm/formats/__init__.py
+++ aamm/formats/__init__.py
@@ -2,8 +2,6 @@ import os
 from itertools import chain
 from typing import Iterable
 
-from aamm.strings import indent
-
 # / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
 
 
@@ -12,26 +10,12 @@ def abbreviate_path(path: str) -> str:
     parts = path.split(os.path.sep)
     if len(parts) < 3:
         return path
-    head, *_, tail = path.split(os.path.sep)
-    return head + os.path.sep + "..." + os.path.sep + tail
+    head, *_, tail = parts
+    return os.path.sep.join((head, "...", tail))
 
 
-def bullets(elements: Iterable, indent_level: int = 0, bullets: str = " - ") -> str:
-    indentation = indent_level * "\t"
-    return indentation + ("\n" + indentation).join(bullets + str(e) for e in elements)
-
-
-def dictionary(
-    dictionary: dict,
-    indent_level: int = 0,
-    line_start: str = "",
-    connector: str = ": ",
-    line_end: str = "\n",
-    end: str = "",
-) -> str:
-    return vectors(
-        dictionary.items(), indent_level, line_start, connector, line_end, end
-    )
+def bullets(iterable: Iterable, prefix: str = " - ", use_repr: bool = False) -> str:
+    return prefix + ("\n" + prefix).join(map(repr if use_repr else str, iterable))
 
 
 def function_call(function_name: str, *args, **kwargs) -> str:
@@ -41,32 +25,7 @@ def function_call(function_name: str, *args, **kwargs) -> str:
     return f"{function_name}({params})"
 
 
-def kwargs(
-    indent_level: int = 0,
-    line_start: str = "",
-    connector: str = " = ",
-    line_end: str = "\n",
-    end: str = "",
-    **kwargs,
-) -> str:
-    return vectors(kwargs.items(), indent_level, line_start, connector, line_end, end)
-
-
 def reprlike(obj: object, **kwargs) -> str:
-    body = indent("\n".join(f"{k}={v!r}," for k, v in kwargs.items()))
-    return f"{type(obj).__qualname__}(\n{body}\n)"
-
-
-def vectors(
-    data: Iterable[Iterable],
-    indent_level: int,
-    line_start: str,
-    connector: str,
-    line_end: str,
-    end: str,
-):
-    line_start = indent_level * "\t" + line_start
-    return (
-        line_end.join(f"{line_start}{connector.join(map(str, vec))}" for vec in data)
-        + end
-    )
+    obj_class = type(obj).__qualname__
+    repr_body = ", ".join(f"{k}={v!r}" for k, v in kwargs.items())
+    return f"{obj_class}({repr_body})"
