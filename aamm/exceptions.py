0b287d0a8dbef88eb14826cfe71a864cbd97ac07
Antonio Medellín <amedellinm@outlook.com>
2024-07-22 01:42:30 -0600

    add name parameter and assert function to DomainError

--- aamm/exceptions.py
+++ aamm/exceptions.py
@@ -1,6 +1,27 @@
 from numbers import Number
 
 
+def assert_domain_error(
+    name: str,
+    value: Number,
+    left: Number,
+    right: Number,
+    closed_left: bool = True,
+    closed_right: bool = True,
+):
+    match closed_left, closed_right:
+        case False, False if left < value < right:
+            return
+        case False, True if left < value <= right:
+            return
+        case True, False if left <= value < right:
+            return
+        case True, True if left <= value <= right:
+            return
+        case _:
+            raise DomainError(name, value, left, right, closed_left, closed_right)
+
+
 def qualname(obj: object) -> str:
     return type(obj).__qualname__
 
@@ -10,6 +31,7 @@ class DomainError(ValueError):
 
     def __init__(
         self,
+        name: str,
         value: Number,
         left: Number,
         right: Number,
@@ -18,7 +40,7 @@ class DomainError(ValueError):
     ) -> None:
         l = self._ends[closed_left]
         r = self._ends[closed_right + 2]
-        super().__init__(f"{value} outside of {l}{left}, {right}{r}")
+        super().__init__(f"'{name}' {value} outside of {l}{left}, {right}{r}")
 
 
 class OperandError(TypeError):
