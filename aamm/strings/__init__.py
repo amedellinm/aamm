49a1b8b8d707cd84d2b0f44835a87e5937234da4
Antonio Medellín <amedellinm@outlook.com>
2024-08-17 01:07:07 -0600

    improve function

--- aamm/strings/__init__.py
+++ aamm/strings/__init__.py
@@ -1,3 +1,9 @@
+import re
+from typing import Iterable
+
+TAB = 4 * " "
+
+
 def is_utf8_valid(string: str) -> bool:
     try:
         string.encode("utf-8")
@@ -6,5 +12,15 @@ def is_utf8_valid(string: str) -> bool:
         return False
 
 
-def indent(string: str, level: int = 1) -> str:
-    return (level * "\t" + string.strip()).replace("\n", "\n" + level * "\t")
+def indent(string: str, level: int = 1, indent_str=TAB) -> str:
+    indent_str *= level
+
+    def repl(match: re.Match) -> str:
+        start, end = match.span()
+        return (end - start) * "\n" + indent_str
+
+    return re.subn(r"(\n)+", repl, indent_str + string)[0]
+
+
+def join_prepend(string: str, iterable: Iterable[str]):
+    return string + string.join(iterable)
