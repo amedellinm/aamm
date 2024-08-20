e5786348797d6e0f42e014c413745304f7d71e6a
Antonio Medellín <amedellinm@outlook.com>
2024-08-20 11:59:41 -0600

    add assert to error messages

--- aamm/testing/asserts.py
+++ aamm/testing/asserts.py
@@ -1,34 +1,34 @@
 def contain(a, b):
-    assert b in a, f"{b!r} in {a!r}"
+    assert b in a, f"assert {b!r} in {a!r}"
 
 
 def equal(a, b):
-    assert a == b, f"{a!r} == {b!r}"
+    assert a == b, f"assert {a!r} == {b!r}"
 
 
 def false(a):
-    assert not a, f"not bool({a!r})"
+    assert not a, f"assert not bool({a!r})"
 
 
 def greater_equal(a, b):
-    assert a >= b, f"{a!r} >= {b!r}"
+    assert a >= b, f"assert {a!r} >= {b!r}"
 
 
 def greater_than(a, b):
-    assert a > b, f"{a!r} > {b!r}"
+    assert a > b, f"assert {a!r} > {b!r}"
 
 
 def less_equal(a, b):
-    assert a <= b, f"{a!r} <= {b!r}"
+    assert a <= b, f"assert {a!r} <= {b!r}"
 
 
 def less_than(a, b):
-    assert a < b, f"{a!r} < {b!r}"
+    assert a < b, f"assert {a!r} < {b!r}"
 
 
 def same(a, b):
-    assert a is b, f"{a!r} is {b!r}"
+    assert a is b, f"assert {a!r} is {b!r}"
 
 
 def true(a):
-    assert a, f"bool({a!r})"
+    assert a, f"assert bool({a!r})"
