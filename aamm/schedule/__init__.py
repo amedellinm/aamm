230dbf04455b90825a675630c67624c258ed76b4
Antonio Medellín <amedellinm@outlook.com>
2024-10-10 02:29:36 -0600

    update docstrings

--- aamm/schedule/__init__.py
+++ aamm/schedule/__init__.py
@@ -18,24 +18,25 @@ def date_range(
 
     PARAMETERS
     ----------
-    ### start: Date
-        * the starting date of the generated range.
-        * the first end is closed, therefore `start` is always included in the range.
+    start:
+        * The starting date of the generated range.
+        * The first end is closed, therefore `start` is always included in the range.
 
-    ### end: Date | int | None = None
-        * the limit of the range.
-        * if `end` is `int`, use `start` shifted `end` days.
-        * if `end` is `datetime.date`, use it directly.
-        * if `end` is `None`, use the value passed as `start` and `start` defaults to
+    end:
+        * The limit of the range.
+        * If `end` is `int`, use `start` shifted `end` days.
+        * If `end` is `datetime.date`, use it directly.
+        * If `end` is `None`, use the value passed as `start`, and `start` defaults to
           `datetime.date.today()`.
 
-    ### step: int = 1
-        * number of days advanced each iteration.
-        * the sign of `step` is inferred, use positive integers only.
+    step:
+        * Number of days advanced each iteration.
+        * The sign of `step` is ignored and inferred from the range.
 
-    ### include_last: bool = True
-        * This argument is only used if `end` is `datetime.date`.
-        * if `True`, turns the range from [start, end) to [start, end].
+    include_last:
+        * If `True`, turns the range from [start, end) to [start, end].
+        * This argument is only used if `end` is `datetime.date`, otherwise, the range
+          is always [start, end).
 
     """
 
@@ -44,19 +45,42 @@ def date_range(
     if isinstance(end, Date):
         end = (end - start).days
         end += include_last * sign(end, zero=1)
-    return (start + DAY * i for i in range(0, end, sign(end, zero=1) * step))
+    return (start + DAY * i for i in range(0, end, sign(end, zero=1) * abs(step)))
 
 
 def find_weekday(
-    date: Date, weekday: int, index: int = 1, include_start: bool = False
+    start_date: Date, weekday: int, index: int = 1, include_start: bool = False
 ) -> Date:
-    if index == 0:
-        return date
+    """
+    DESCRIPTION
+    -----------
+    Find the nth date whose weekday matches `weekday`.
 
-    shift = weekday - date.weekday()
+    PARAMETERS
+    ----------
+    start_date:
+        * The starting date of the search.
+
+    weekday:
+        * An `int` representing the day of the week.
+        * Ranges from [Monday = 0, Sunday = 6].
+
+    index:
+        * The index of the weekday to find.
+        * Can be positive or negative but not 0.
+
+    include_start:
+        * If `True`, `start_date` counts for the indexing of the returned date.
+
+    """
+
+    if index == 0:
+        raise ValueError("argument `index` can not be 0")
+
+    shift = weekday - start_date.weekday()
     index_sign = sign(index)
     shift_sign = sign(shift)
     index -= (shift_sign == 0 and include_start) * index_sign
     shift += (shift_sign != index_sign) * 7 * index
 
-    return date + DAY * shift
+    return start_date + DAY * shift
