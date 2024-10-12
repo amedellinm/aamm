baf931a37faa6e1ee8a7b1050e5212b8bd98c7e2
Antonio Medellín <amedellinm@outlook.com>
2024-10-11 23:04:13 -0600

    normalize stack_index

--- aamm/file_system.py
+++ aamm/file_system.py
@@ -24,9 +24,9 @@ def are_directories_equal(d1: str, d2: str) -> bool:
 def current_file(
     extension: str | EllipsisType | None = ...,
     name_only: bool = False,
-    stack_index: int = 1,
+    stack_index: int = 0,
 ) -> str:
-    file = inspect.stack()[stack_index].filename
+    file = inspect.stack()[stack_index + 1].frame.f_globals["__file__"]
     if name_only:
         file = os.path.basename(file)
 
@@ -40,15 +40,15 @@ def current_file(
     return name + "." + extension
 
 
-def current_folder(name_only: bool = False, stack_index: int = 1) -> str:
-    folder_path = os.path.dirname(inspect.stack()[stack_index].filename)
-    return os.path.basename(folder_path) if name_only else folder_path
+def current_directory(name_only: bool = False, stack_index: int = 0) -> str:
+    directory_path = os.path.dirname(current_file(stack_index=stack_index + 1))
+    return os.path.basename(directory_path) if name_only else directory_path
 
 
-def dir_up(path: str | None = None, n: int = 1, stack_index: int = 2) -> str:
+def dir_up(path: str | None = None, n: int = 1, stack_index: int = 0) -> str:
     """Returns `n` directories up the given path."""
     if path is None:
-        path = current_folder(stack_index=stack_index)
+        path = current_directory(stack_index=stack_index + 1)
     elif os.path.isfile(path):
         path = os.path.dirname(path)
     for _ in range(n):
@@ -57,11 +57,11 @@ def dir_up(path: str | None = None, n: int = 1, stack_index: int = 2) -> str:
 
 
 def files(
-    path: str | None = None, names_only: bool = False, stack_index: int = 2
+    path: str | None = None, names_only: bool = False, stack_index: int = 0
 ) -> list[str]:
     """List all files in `path`."""
     if path is None:
-        path = current_folder(stack_index=stack_index)
+        path = current_directory(stack_index=stack_index + 1)
     try:
         root, _, subpaths = next(os.walk(path))
     except StopIteration:
@@ -75,12 +75,12 @@ def file_name(path: str) -> tuple[str, str]:
     return os.path.splitext(os.path.basename(path))
 
 
-def folders(
-    path: str | None = None, names_only: bool = False, stack_index: int = 2
+def directories(
+    path: str | None = None, names_only: bool = False, stack_index: int = 0
 ) -> list[str]:
-    """List all folders in `path`."""
+    """List all directories in `path`."""
     if path is None:
-        path = current_folder(stack_index=stack_index)
+        path = current_directory(stack_index=stack_index + 1)
     try:
         root, subpaths, _ = next(os.walk(path))
     except StopIteration:
@@ -96,9 +96,9 @@ def has_extension(path: str, extension: str = None) -> bool:
     return bool(ext) if extension is None else extension == ext
 
 
-def here(file_name: str = "", stack_index: int = 2) -> str:
+def here(file_name: str = "", stack_index: int = 0) -> str:
     """Constructs the path of a file in the current directory."""
-    return os.path.join(current_folder(stack_index=stack_index), file_name)
+    return os.path.join(current_directory(stack_index=stack_index + 1), file_name)
 
 
 def search(
@@ -106,25 +106,25 @@ def search(
     condition: str | Callable | None = None,
     use_complement: bool = False,
     use_breadth_first: bool = False,
-    stack_index: int = 2,
+    stack_index: int = 0,
 ) -> Iterator[str]:
-    """From `root`, depth-first traverses folders according to `condition`."""
+    """From `root`, depth-first traverses directories according to `condition`."""
     if root is None:
-        root = current_folder(stack_index=stack_index)
+        root = current_directory(stack_index=stack_index + 1)
     elif root is ...:
         root = os.getcwd()
 
     explore = breadth_first if use_breadth_first else depth_first
 
     if condition is None:
-        return explore(root, expand=folders)
+        return explore(root, expand=directories)
 
     if isinstance(condition, str):
         condition = match.create_matcher(condition)
 
     def expand(node):
         if condition(node) ^ use_complement:
-            return folders(node)
+            return directories(node)
         return ()
 
     return explore(root, expand)
