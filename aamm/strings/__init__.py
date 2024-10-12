19d6ab144f51c3f898c6a1eca0699f90ef0b5983
Antonio Medellín <amedellinm@outlook.com>
2024-10-11 23:05:53 -0600

    remove function

--- aamm/strings/__init__.py
+++ aamm/strings/__init__.py
@@ -1,26 +1,23 @@
 import re
-from typing import Iterable
 
 TAB = 4 * " "
 
 
-def is_utf8_valid(string: str) -> bool:
-    try:
-        string.encode("utf-8")
-        return True
-    except UnicodeEncodeError:
-        return False
-
-
 def indent(string: str, level: int = 1, indent_str=TAB) -> str:
+    """Indent a string on each series of newlines."""
     indent_str *= level
 
     def repl(match: re.Match) -> str:
         start, end = match.span()
         return (end - start) * "\n" + indent_str
 
-    return re.subn(r"(\n)+", repl, indent_str + string)[0]
+    return re.subn(r"\n+", repl, indent_str + string)[0]
 
 
-def join_prepend(string: str, iterable: Iterable[str]):
-    return string + string.join(iterable)
+def is_utf8_valid(string: str) -> bool:
+    """Validate `string` is utf-8 encodable."""
+    try:
+        string.encode("utf-8")
+        return True
+    except UnicodeEncodeError:
+        return False
