6989ccbb1f2a359e9364a3d9143db2f881c13bfd
Antonio Medellín <amedellinm@outlook.com>
2024-10-26 11:01:52 -0600

    move exception messages to their own module

--- aamm/logtools/formats.py
+++ aamm/logtools/formats.py
@@ -1,12 +1,6 @@
 from itertools import chain
 
-
-def _qualname(obj: object) -> str:
-    return type(obj).__qualname__
-
-
-def attribute_error(obj: object, attribute: str) -> str:
-    return f"{_qualname(obj)!r} object has no attribute {attribute!r}"
+from aamm.strings import indent
 
 
 def function_call(function_name: str, *args, **kwargs) -> str:
@@ -16,29 +10,5 @@ def function_call(function_name: str, *args, **kwargs) -> str:
     return f"{function_name}({params})"
 
 
-def index_error(sequence: object, index: int) -> str:
-    length = len(sequence)
-    type_name = _qualname(sequence)
-    return f"index {index} out of range for {type_name!r} object of length {length}"
-
-
-def key_error(key: object, mapping: object) -> str:
-    return f"{key!r} not in {_qualname(mapping)!r} object"
-
-
-def operand_error(operator: str, *operands: tuple[object]) -> str:
-    operands = ", ".join(map(repr, operands))
-    return f"invalid operator {operator!r} for operand(s): {operands}"
-
-
-def reprlike(obj: object, **kwargs) -> str:
-    obj_class = type(obj).__qualname__
-    repr_body = ", ".join(f"{k}={v!r}" for k, v in kwargs.items())
-    return f"{obj_class}({repr_body})"
-
-
-def type_error(obtained: object, expected: type | tuple[type]) -> str:
-    if isinstance(expected, type):
-        expected = (expected,)
-    types = " | ".join(t.__qualname__ for t in expected)
-    return f"expected type(s) {types}, got {_qualname(obtained)}"
+def tag_header_body(tag, header, body):
+    return f"[{tag}] {header}\n{indent(body)}"
