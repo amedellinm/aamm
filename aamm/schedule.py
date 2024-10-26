5ca1e49eb5b10940c3cebabce81df596423062f5
Antonio Medellín <amedellinm@outlook.com>
2024-10-26 11:12:40 -0600

    unify subpackage in a single module

--- aamm/schedule.py
+++ aamm/schedule.py
@@ -1,17 +1,113 @@
 from calendar import monthrange
 from datetime import date as Date
+from datetime import timedelta as TimeDelta
 from typing import Iterator, Self
 
-import aamm.strings.match as match
-from aamm.logtools.formats import index_error, operand_error, type_error
+from aamm.exception_message import index_error, operand_error, type_error
 from aamm.meta import ReadOnlyProperty
 from aamm.std import qualname, sign
+from aamm.strings import pattern_match
+
+# / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
+
+
+DAY = TimeDelta(days=1)
+WEEK = TimeDelta(weeks=1)
+
+DATE0 = Date(2000, 1, 1)
+
+
+# / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
+
+
+def date_range(
+    start: Date,
+    end: Date | int | None = None,
+    step: int = 1,
+    include_last: bool = True,
+) -> Iterator[Date]:
+    """
+    DESCRIPTION
+    -----------
+    Generate a range of dates from `start` to `end` advancing `step` dates at a time.
+
+    PARAMETERS
+    ----------
+    start:
+        * The starting date of the generated range.
+        * The first end is closed, therefore `start` is always included in the range.
+
+    end:
+        * The limit of the range.
+        * If `end` is `int`, use `start` shifted `end` days.
+        * If `end` is `datetime.date`, use it directly.
+        * If `end` is `None`, use the value passed as `start`, and `start` defaults to
+          `datetime.date.today()`.
+
+    step:
+        * Number of days advanced each iteration.
+        * The sign of `step` is ignored and inferred from the range.
+
+    include_last:
+        * If `True`, turns the range from [start, end) to [start, end].
+        * This argument is only used if `end` is `datetime.date`, otherwise, the range
+          length is always `end`.
+
+    """
+
+    if end is None:
+        start, end = Date.today(), start
+    if isinstance(end, Date):
+        end = (end - start).days
+        end += include_last * sign(end, zero=1)
+    return (start + DAY * i for i in range(0, end, sign(end, zero=1) * abs(step)))
+
+
+def find_weekday(
+    start_date: Date, weekday: int, index: int = 1, include_start: bool = False
+) -> Date:
+    """
+    DESCRIPTION
+    -----------
+    Find the nth date whose weekday matches `weekday`.
+
+    PARAMETERS
+    ----------
+    start_date:
+        * The starting date of the search.
+
+    weekday:
+        * An `int` representing the day of the week.
+        * Ranges from [Monday = 0, Sunday = 6].
+
+    index:
+        * The index of the weekday to find.
+        * Can be positive or negative but not 0.
+
+    include_start:
+        * If `True`, `start_date` counts for the indexing of the returned date.
+
+    """
+
+    if index == 0:
+        raise ValueError("argument `index` can not be 0")
+
+    shift = weekday - start_date.weekday()
+    index_sign = sign(index)
+    shift_sign = sign(shift)
+    index -= (shift_sign == 0 and include_start) * index_sign
+    shift += (shift_sign != index_sign) * 7 * index
+
+    return start_date + DAY * shift
+
+
+# / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
 
 
 class YearMonth:
     """Date-like object, holds a pair year-month."""
 
-    is_yearmonth_string = match.create_matcher(r"^\d{4}(0[1-9]|1[0-2])$")
+    is_yearmonth_string = pattern_match.create_matcher(r"^\d{4}(0[1-9]|1[0-2])$")
     value = ReadOnlyProperty()
 
     def __add__(self, other: int) -> Self:
--- aamm/schedule/constants.py
+++
@@ -1,7 +0,0 @@
-from datetime import date as Date
-from datetime import timedelta as TimeDelta
-
-DAY = TimeDelta(days=1)
-WEEK = TimeDelta(weeks=1)
-
-DATE0 = Date(2000, 1, 1)
