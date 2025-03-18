0177d34edea5990eaa3a21263a99519927b7e18f
Antonio Medellín <amedellinm@outlook.com>
2025-03-18 08:36:01 -0600

    update import

--- aamm/scripts/missing_test_files.py
+++ aamm/scripts/missing_test_files.py
@@ -1,9 +1,8 @@
 import sys
 
 from aamm import file_system as fs
-from aamm import metadata
+from aamm import meta, metadata
 from aamm.logging import Logger
-from aamm.logging import formats as fmts
 from aamm.testing.core import test_file
 
 
@@ -18,7 +17,7 @@ def main():
     with fs.cwd_context(metadata.home):
         for file in metadata.header_files:
             if not fs.exists(missing_test_file := test_file(file)):
-                module = fmts.module_identifier(fs.relative(file))
+                module = meta.module_identifier(fs.relative(file))
                 missing_test_files[module] = missing_test_file
 
     # Handle successful case.
